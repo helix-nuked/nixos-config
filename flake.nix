d8596996656a1647bd8cae7c880f6a23729ae442
helix-nuked <helix.nuked@proton.me>
2025-01-02 13:42:04 +0500

    extra bruh

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       # nix community's cache server
       "https://nix-community.cachix.org"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       # nix community's cache server public key
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
