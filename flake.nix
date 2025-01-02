0c883e7d8e347a1ad4b2bf0dcd8a5e6b91247706
helix-nuked <helix.nuked@proton.me>
2025-01-02 11:53:18 +0500

    add cache servers cuz speed and community better

--- flake.nix
+++ flake.nix
@@ -1,6 +1,27 @@
 {
   description = "Flake to connect configuration.nix";
 
+  # the nixConfig here only affects the flake itself, not the system configuration!
+  nixConfig = {
+    # override the default substituters
+    substituters = [
+      # cache mirror located in China
+      # status: https://mirror.sjtu.edu.cn/
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      # status: https://mirrors.ustc.edu.cn/status/
+      # "https://mirrors.ustc.edu.cn/nix-channels/store"
+
+      "https://cache.nixos.org"
+
+      # nix community's cache server
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      # nix community's cache server public key
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
@@ -31,6 +52,11 @@
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
+        }
+        {
+            # given the users in this list the right to specify additional substituters via:
+            #    1. `nixConfig.substituters` in `flake.nix`
+            nix.settings.trusted-users = [ "ryan" ];
         }
       ];
     };
