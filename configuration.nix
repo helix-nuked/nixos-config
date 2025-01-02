235386b9a4d82ad4f2abcc3f4f19aed497ad8137
helix-nuked <helix.nuked@proton.me>
2025-01-02 18:09:36 +0500

    remove giperland-plugins

--- configuration.nix
+++ configuration.nix
@@ -102,7 +102,8 @@
       enable = true;
       # Whether to enable XWayland
       xwayland.enable = true;
-      package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+      package = inputs.hyprland.packages."${pkgs.stdenv.hostPlatform.system}".hyprland;
+      portalPackage = inputs.hyprland.packages.$(pkgs.stdenv.hostPlatform.system).xdg-desktop-portal-hyprland;
     };
   };
 
