f760086580dc514520270972dba578355dbd113f
helix-nuked <helix.nuked@proton.me>
2025-01-02 19:13:00 +0500

    testing begins here with trying plugins

--- flake.nix
+++ flake.nix
@@ -42,10 +42,10 @@
         inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland.url = "github:hyprwm/Hyprland";
-    # hyprland-plugins = {
-    #   url = "github:hyprwm/hyprland-plugins";
-    #   inputs.hyprland.follows = "hyprland";
-    # };
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, nixvim, hyprland, ... }: {
