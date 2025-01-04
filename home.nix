36e2f606e5421a994e76f71773a7ea1984df21e9
isroiljon2303 <sinifdoshlarserial@gmail.com>
2025-01-04 13:30:54 +0500

    big change and needs rework on categorization but still fine :thumbsup (and also misc category needs logical thinking

--- home.nix
+++ home.nix
@@ -37,14 +37,6 @@
       package = pkgs.firefox-wayland;
     };
 
-    nh = {
-      enable = true;
-      clean.enable = true;
-      clean.extraArgs = "--keep-since 1w";
-      clean.dates = "weekly";
-      flake = "/home/helix/nixos-config";
-    };
-
     nixvim = {
       enable = true;
       defaultEditor = true;
---
+++ nixos/modules/misc.nix
@@ -0,0 +1,12 @@
+{ lib, config, ... }
+
+{
+    options = {
+        misc.cups.enable =
+            lib.mkEnableOption "enables cups";
+    };
+
+    config = lib.mkIf config.misc.cups.enable {
+        import = ./misc/cups.nix;
+    };
+}
