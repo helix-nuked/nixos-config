4957466db61843d23a7307eec07facb7b68a4c0c
helix-nuked <helix.nuked@proton.me>
2025-01-05 20:17:59 +0500

    checking later

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,7 @@
   };
 
   networking = {
-    hostName = "helinix"; # Define your hostname.
+    hostName = "acer"; # Define your hostname.
     # Pick only one of the below networking options.
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networkmanager.enable = true;  # Easiest to use and most distros use this by default.
