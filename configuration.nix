7a1f16304bbd31457f7313f13f69c5d888ec3343
helix-nuked <helix.nuked@proton.me>
2025-01-06 10:34:12 +0500

    add zsh for system as back up

--- configuration.nix
+++ configuration.nix
@@ -153,6 +153,8 @@
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
 
+  programs.zsh.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.helix = {
     isNormalUser = true;
